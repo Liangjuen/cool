a13b9d2dcb12f5f741e906c35beebd5b2fda52f8
梁举恩 <3103421940@qq.com>
2024-04-19 19:10:27 +0800

    ✨ feat: update ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common'
+import {
+	Module,
+	NestModule,
+	MiddlewareConsumer,
+	RequestMethod
+} from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule, TypeOrmModule } from '@/globalModules'
+import { UserModule } from '@/modules/user/user.module'
+import { LoggerMiddleware } from '@/middleware'
 
 @Module({
-	imports: [ConfigModule, TypeOrmModule],
+	imports: [ConfigModule, TypeOrmModule, UserModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+			.apply(LoggerMiddleware)
+			.forRoutes({ path: '*', method: RequestMethod.ALL })
+	}
+}
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,46 @@
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Patch,
+	Param,
+	Delete,
+	ParseIntPipe
+} from '@nestjs/common'
+import { UserService } from './user.service'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+
+@Controller('users')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Post()
+	create(@Body() createUserDto: CreateUserDto) {
+		return this.userService.create(createUserDto)
+	}
+
+	@Get()
+	findAll() {
+		return this.userService.findAll()
+	}
+
+	@Get(':id')
+	findOne(@Param('id', ParseIntPipe) id: number) {
+		return this.userService.findOne(+id)
+	}
+
+	@Patch(':id')
+	update(
+		@Param('id', ParseIntPipe) id: number,
+		@Body() updateUserDto: UpdateUserDto
+	) {
+		return this.userService.update(+id, updateUserDto)
+	}
+
+	@Delete(':id')
+	remove(@Param('id', ParseIntPipe) id: number) {
+		return this.userService.remove(+id)
+	}
+}
