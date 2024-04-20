a9173bae34548573051c676ca41cde5e13d75b94
梁举恩 <3103421940@qq.com>
2024-04-20 21:45:03 +0800

    🦄 refactor: tree shaking

--- src/config/guard.ts
+++
@@ -1,8 +0,0 @@
-export const guard = {
-	role: {
-		metadataKey: 'role'
-	},
-	permission: {
-		metadataKey: 'permission'
-	}
-}
--- src/modules/user/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, IsNotEmpty } from 'class-validator'
-import { PartialType } from '@nestjs/mapped-types'
-import { CreateUserDto } from './create-user.dto'
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-	@IsNumber()
-	@IsNotEmpty()
-	id: number
-}
