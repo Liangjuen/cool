f65c2f8d051580ed244dca44b1a6f990f313c4f1
梁举恩 <3103421940@qq.com>
2024-04-20 21:53:21 +0800

    ✨ feat: 启用全局身份认证 AuthGuard 守卫

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common'
 import { DataSource } from 'typeorm'
+import { APP_GUARD } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ConfigModule, TypeOrmModule } from '@/globalModules'
-import { UserModule } from '@/modules/user/user.module'
+import { ConfigModule, TypeOrmModule, JwtModule } from '@/globalModules'
+import { UsersModule } from '@/modules/users/users.module'
+import { AuthModule } from '@/modules/auth/auth.module'
 import { LoggerMiddleware } from '@/middleware'
+import { AuthGuard } from '@/guard'
 
 @Module({
-	imports: [ConfigModule, TypeOrmModule, UserModule],
+	imports: [ConfigModule, TypeOrmModule, JwtModule, AuthModule, UsersModule],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: AuthGuard
+		}
+	]
 })
 export class AppModule implements NestModule {
 	constructor(private dataSource: DataSource) {}
