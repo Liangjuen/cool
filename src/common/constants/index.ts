35a6ec84a91f68798741650cf00cdbc978193eb3
梁举恩 <3103421940@qq.com>
2024-04-26 23:47:02 +0800

    ✨ feat: update ...

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -3,3 +3,5 @@ export const IS_PUBLIC_KEY = 'IsPublic'
 export const ROLE_GUARD_METADATA_KEY = 'RoleGuardMetadata'
 
 export const PERMISSION_GUARD_METADATA_KEY = 'PermissionGuardMetadata'
+
+export const ROLE_CHANGED_EVENT = 'role.changed'
--- src/globalModules/index.ts
+++ src/globalModules/index.ts
@@ -1,9 +1,12 @@
 import CM from './config.module'
 import TOM from './typeorm.module'
 import JWTM from './jwt.module'
-export { RedisCacheModule } from './redis-cache/redis-cache.module'
-export { RedisCacheService } from './redis-cache/redis-cache.service'
+import EEM from './event.emitter.module'
+export * from './redis-cache/redis-cache.module'
+export * from './redis-cache/redis-cache.service'
+export * from './logger.module'
 
 export const ConfigModule = CM
 export const TypeOrmModule = TOM
 export const JwtModule = JWTM
+export const EventEmitterModule = EEM
