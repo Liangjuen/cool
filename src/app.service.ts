319fe6b0c52ed1886294903815361817e6534e42
梁举恩 <3103421940@qq.com>
2024-04-26 23:47:29 +0800

    ✨ feat: Permission Guard 实现

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,17 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
+import { RoleCacheService } from '@/modules/base/roles/cache/cache.service'
 
 @Injectable()
-export class AppService {
+export class AppService implements OnApplicationBootstrap {
+	constructor(private readonly roleCacheService: RoleCacheService) {}
+
+	/**
+	 * 应用初始化完成执行
+	 */
+	onApplicationBootstrap() {
+		this.roleCacheService.setCache()
+	}
+
 	getHello(): string {
 		return 'Hello World!'
 	}
--- src/guard/permission/permission.guard.ts
+++ src/guard/permission/permission.guard.ts
@@ -1,18 +1,42 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
+import { Request } from 'express'
 import { Reflector } from '@nestjs/core'
-import { Observable } from 'rxjs'
 import { PERMISSION_GUARD_METADATA_KEY } from '@/common/constants'
+import { RoleCacheService } from '@/modules/base/roles/cache'
+import { ROLE } from '@/common/enums'
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
-	constructor(private Reflector: Reflector) {}
-	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-		const permission = this.Reflector.get<string[]>(
+	constructor(
+		private reflector: Reflector,
+		private roleCacheService: RoleCacheService
+	) {}
+	async canActivate(context: ExecutionContext): Promise<boolean> {
+		const permission = this.reflector.get<string[]>(
 			PERMISSION_GUARD_METADATA_KEY,
 			context.getHandler()
 		)
-		console.log('[PermissionGuard]:', permission)
 
-		return true
+		const payload: Payload = context.switchToHttp().getRequest<Request>()['user']
+
+		// 超管放行
+		if (payload.roles.includes(ROLE.Admin)) return true
+
+		console.log(payload)
+
+		// 获取角色权限列表
+		const rolePerms = await this.roleCacheService.getCache()
+
+		let isAuth = false
+
+		// 查看当前权限控制点是否在角色权限列表中
+		payload.roles.forEach(r => {
+			if (rolePerms[r]) {
+				if (isAuth) return
+				isAuth = rolePerms[r].includes(permission)
+			}
+		})
+
+		return isAuth
 	}
 }
