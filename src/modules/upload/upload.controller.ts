f45b3562289ac5487f6949a4366f01c6670e7134
梁举恩 <3103421940@qq.com>
2024-05-10 18:41:54 +0800

    📃 docs: add api oprations

--- src/modules/upload/upload.controller.ts
+++ src/modules/upload/upload.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common'
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express'
 import { Express, Request } from 'express'
+import { ApiTags, ApiOperation } from '@nestjs/swagger'
 import { Permission } from '@/common/decorator'
 import { UploadService } from './upload.service'
 import { uploadStorage } from './upload.storage'
@@ -20,23 +21,27 @@ import { UploadDto } from './upload.dto'
 import { USERPAYLOAD } from '@/common/constants'
 import { PERM } from '@/common/permissions'
 
+@ApiTags('上传')
 @UseGuards(PermissionGuard)
 @Controller()
 export class UploadController {
 	constructor(private readonly uploadService: UploadService) {}
 
+	@ApiOperation({ summary: '获取上传模式' })
 	@Permission(PERM.Upload.Mode)
 	@Get('upload/mode')
 	getUploadMode() {
 		return this.uploadService.getUploadMode()
 	}
 
+	@ApiOperation({ summary: '获取七牛云上传授权' })
 	@Permission(PERM.Upload.QiniuAuth)
 	@Get('upload/qiniu/authorization')
 	uploadQiniuAuthorization() {
 		return this.uploadService.uploadQiniuAuthorization()
 	}
 
+	@ApiOperation({ summary: '单文件上传' })
 	@Post('upload')
 	@Permission(PERM.Upload.Local)
 	@UseGuards(LocalUploadGuard)
@@ -49,6 +54,7 @@ export class UploadController {
 		return this.uploadService.saveFile(file, { user: req[USERPAYLOAD], ...dto })
 	}
 
+	@ApiOperation({ summary: '多文件上传' })
 	@Post('uploads')
 	@Permission(PERM.Upload.Local)
 	@UseInterceptors(FilesInterceptor('file', undefined, { storage: uploadStorage() }))
