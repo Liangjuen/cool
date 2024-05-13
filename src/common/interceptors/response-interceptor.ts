5e1a16f49871b809fdffea7b4ce37421a4b3f5e8
梁举恩 <3103421940@qq.com>
2024-05-14 01:16:44 +0800

    ✨ feat: 响应结果支持自定义

--- src/common/interceptors/response-interceptor.ts
+++ src/common/interceptors/response-interceptor.ts
@@ -1,7 +1,8 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common'
-import { Request, Response } from 'express'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
+import { ResponseModel } from '@/common/classes'
+import { ResponseCode } from '@/common/enums'
 
 /**
  * 定义接口返回的数据格式
@@ -9,16 +10,12 @@ import { map } from 'rxjs/operators'
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, API.Response<T>> {
 	intercept(context: ExecutionContext, next: CallHandler): Observable<API.Response<T>> {
-		const request = context.switchToHttp().getRequest<Request>()
-		const response = context.switchToHttp().getResponse<Request>()
-
 		return next.handle().pipe(
 			map(data => {
-				return {
-					data,
-					code: 1,
-					message: 'ok'
-				}
+				// 判断是否为响应模型实例，是则直接返回
+				if (data instanceof ResponseModel) return data
+
+				return new ResponseModel(ResponseCode.SUCCESS, data)
 			})
 		)
 	}
